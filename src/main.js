416937e15e068537ec127359c24764d8678d3f6b
Chris Schweda <cschweda@gmail.com>
2017-12-21 14:26:06 -0600

    Remove fusioncharts from bundle

--- src/main.js
+++ src/main.js
@@ -6,16 +6,15 @@ import jQuery from 'jquery';
 
 
 
-import VueFusionCharts from 'vue-fusioncharts';
-import FusionCharts from 'fusioncharts'
-import Charts from 'fusioncharts/fusioncharts.charts'
-import Maps from 'fusioncharts/fusioncharts.maps'
-
-Charts(FusionCharts);
-Maps(FusionCharts);
+// import VueFusionCharts from 'vue-fusioncharts';
+// import FusionCharts from 'fusioncharts'
+// import Charts from 'fusioncharts/fusioncharts.charts'
+// import Maps from 'fusioncharts/fusioncharts.maps'
 import IllinoisMap from '../static/vendor/fusioncharts/maps/fusioncharts.illinois.js';
+// Charts(FusionCharts);
+// Maps(FusionCharts);
 IllinoisMap(FusionCharts);
-Vue.use(VueFusionCharts, VueFusionCharts);
+// Vue.use(VueFusionCharts,VueFusionCharts);
 
 var APPS = {
     Illinois
